f9b7d864b7ffc0b069a2178e838c077d78412a49
Haovi Zhang <yukiniro@hotmail.com>
2025-07-20 11:44:08 +0800

    feat: 移除多个视频处理页面组件，包括提取音频、GIF 转视频、视频压缩、视频裁剪和视频转码

--- src/app/[locale]/extract-audio/page.tsx
+++
@@ -1,9 +0,0 @@
-import { ToolPageTemplate } from '@/components/tool-page-template'
-
-export default function ExtractAudioPage() {
-  return (
-    <ToolPageTemplate
-      toolKey="extractAudio"
-    />
-  )
-}
--- src/app/[locale]/gif-to-video/page.tsx
+++
@@ -1,9 +0,0 @@
-import { ToolPageTemplate } from '@/components/tool-page-template'
-
-export default function GifToVideoPage() {
-  return (
-    <ToolPageTemplate
-      toolKey="gifToVideo"
-    />
-  )
-}
